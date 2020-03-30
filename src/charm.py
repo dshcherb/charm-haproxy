68201f573bca2f01b360b19ae7b8a1b2a2e73e63
Dmitrii Shcherbakov <dmitrii.shcherbakov@canonical.com>
2020-03-31 00:33:51 +0300

    Add HA support via keepalived
    * haproxy can now be related to the keepalived-vrrp charm to enable
      VIP-based HA;
    * health-checks of haproxy itself are basic port connection checks for
      now;
    * haproxy is now reconfigured on config-changed events.
    
    Port selection for a vip is currently based on the "website" endpoint
    which can be improved. The vip itself is configurable on the haproxy
    charm and is passed down to keepalived along with other relevant options
    such as health-checks.

--- src/charm.py
+++ src/charm.py
@@ -10,11 +10,16 @@ import subprocess
 from ops.charm import CharmBase
 from ops.main import main
 from ops.framework import StoredState
-from ops.model import ActiveStatus
+from ops.model import ActiveStatus, BlockedStatus
 
 from jinja2 import Environment, FileSystemLoader
 from pathlib import Path
 from interface_proxy_listen_tcp import ProxyListenTcpInterfaceRequires
+from interface_vrrp_parameters import (
+    VRRPParametersProvides,
+    VRRPInstance,
+    VRRPScript,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -37,6 +42,10 @@ class HaproxyCharm(CharmBase):
 
         self.tcp_backends = ProxyListenTcpInterfaceRequires(self, 'proxy-listen-tcp')
         self.framework.observe(self.tcp_backends.on.backends_changed, self.on_backends_changed)
+
+        self.keepalived = VRRPParametersProvides(self, 'vrrp-parameters')
+        self.framework.observe(self.keepalived.on.keepalived_available,
+                               self.on_keepalived_available)
 
         self.state.set_default(started=False)
 
@@ -65,10 +74,15 @@ class HaproxyCharm(CharmBase):
             self.state.started = False
 
     def on_config_changed(self, event):
-        # TODO: handle real global config changes.
+        self.reconfigure_haproxy()
+        self.reconfigure_keepalived()
         subprocess.check_call(['systemctl', 'restart', 'haproxy'])
 
     def on_backends_changed(self, event):
+        self.reconfigure_haproxy()
+        self.reconfigure_keepalived()
+
+    def reconfigure_haproxy(self):
         ctxt = {
             'listen_proxies': self.tcp_backends.listen_proxies
         }
@@ -76,8 +90,37 @@ class HaproxyCharm(CharmBase):
         template = env.get_template('haproxy.conf.j2')
         rendered_content = template.render(ctxt)
         self.haproxy_conf_file.write_text(rendered_content)
-
         subprocess.check_call(['systemctl', 'restart', 'haproxy'])
+
+    def on_keepalived_available(self, event):
+        if not self.state.started:
+            event.defer()
+        self.reconfigure_keepalived()
+
+    def reconfigure_keepalived(self):
+        if not self.keepalived.is_joined:
+            return
+
+        # TODO: the check source into a separate file.
+        vrrp_scripts = []
+        for port in self.tcp_backends.frontend_ports:
+            vrrp_scripts.append(VRRPScript(f'haproxy_port_{port}_check',
+                                           f'''script "bash -c '</dev/tcp/127.0.0.1/{port}'"'''))
+        # TODO: there needs to be a better way to determine an egress-facing network interface
+        # on which to configure a virtual IP than this.
+        vip_interface = self.model.get_binding('website').network.interfaces[0].name
+        virtual_ip = self.model.config.get('virtual-ip')
+        if virtual_ip is None:
+            self.unit.status = BlockedStatus('Waiting for an administrator to set virtual-ip.')
+            return
+        vrrp_instance = VRRPInstance(self.app.name,
+                                     self.model.config['virtual-router-id'],
+                                     [virtual_ip],
+                                     vip_interface,
+                                     track_interfaces=[vip_interface],
+                                     track_scripts=vrrp_scripts)
+        self.keepalived.configure_vrrp_instances([vrrp_instance])
+        self.unit.status = ActiveStatus()
 
 
 if __name__ == '__main__':
