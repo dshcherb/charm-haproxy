2f1a113dd8aecf6a2f320f551ac59efbad788c5e
Dmitrii Shcherbakov <dmitrii.shcherbakov@canonical.com>
2020-03-30 11:21:12 +0300

    Add a simple unit test
    With the current revision of the operator framework it is currently
    broken and raises
    
    RuntimeError: cannot find StoredVariable attribute in type HaproxyCharm

---
+++ test/test_charm.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import unittest
+import sys
+
+sys.path.append('lib') # noqa
+sys.path.append('src') # noqa
+
+from src import charm
+from ops.testing import Harness
+
+from unittest.mock import patch, call
+
+
+class TestTestingModelBackend(unittest.TestCase):
+
+    @patch('pathlib.Path.write_text')
+    @patch('subprocess.check_call')
+    def test_install(self, mock_check_call, write_text):
+        harness = Harness(charm.HaproxyCharm)
+        harness.begin()
+        harness.charm.on.install.emit()
+
+        mock_check_call.assert_has_calls([
+            call(['apt', 'update']),
+            call(['apt', 'install', '-yq', 'haproxy'])
+        ])
+
+        harness.charm.HAPROXY_ENV_FILE.write_text.assert_called()
+        harness.charm.haproxy_conf_file.write_text.assert_called()
+
+
+if __name__ == '__main__':
+    unittest.main()
